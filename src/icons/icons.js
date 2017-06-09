f922e7bdb196928daa80dd9aa7625729fbc8b188
Martin Banks <anotherbanksy@gmail.com>
2017-06-09 17:49:55 +1000

    add icon files

---
+++ src/icons/icons.js
@@ -0,0 +1,4 @@
+export { default as menu_black } from './menuIcon_black.png'
+export { default as angle_right_dark } from './angle_right_dark.png'
+
+// export { default as menu_white } from './menuIcon_white.png'
